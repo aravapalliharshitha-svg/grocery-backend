6d1ff61b008c9f2f97365a10d04a7e1b75b95cba
aravapalli. sai siri harshitha <aravapalliharshitha@gmail.com>
2026-04-12 18:55:21 +0530

    Fixed folder casing and CORS for live deployment

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 //  MIDDLEWARE
 app.use(express.json()); 
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: '*', 
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'x-auth-token']
 }));
