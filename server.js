d2887cf394fc6503f915054fe2438d81da8916b0
aravapalli. sai siri harshitha <aravapalliharshitha@gmail.com>
2026-04-12 19:33:58 +0530

    Fix port binding for Render

--- server.js
+++ server.js
@@ -34,4 +34,8 @@ app.get('/', (req, res) => {
 
 //  START SERVER
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+    console.log("Connected to MongoDB Warehouse! 🥗");
+});
